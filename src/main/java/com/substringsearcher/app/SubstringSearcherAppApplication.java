f663f4d1931497b3f942042eb247773c6e337620
Andrzej Bobak <a.bobak@interia.pl>
2022-11-09 00:32:42 +0100

    Project startup

---
+++ src/main/java/com/substringsearcher/app/SubstringSearcherAppApplication.java
@@ -0,0 +1,13 @@
+package com.substringsearcher.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubstringSearcherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubstringSearcherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/substringsearcher/app/SubstringSearcherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.substringsearcher.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubstringSearcherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
