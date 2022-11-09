18e92fa20e55bbf0aae18d4ff6964205ee624e39
Andrzej Bobak <a.bobak@interia.pl>
2022-11-09 14:35:21 +0100

    Initial dummy controller and healthcheck controller

---
+++ src/main/java/com/substringsearcher/app/controller/HealthCheck.java
@@ -0,0 +1,17 @@
+package com.substringsearcher.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "OK at " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+}
